56daeba9c07b93b3954959e9ed16508d38e7778a
jgierlach <jegierlach@gmail.com>
2018-10-09 14:34:42 -0400

    mobile gradient background is working

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+      <div className='ui menu white index'>
+        <a className='item'>Reviews</a>
+        <a className='item'>Upcoming Events</a>
+      </div>   
+    )
+}
+
+export default Navbar
