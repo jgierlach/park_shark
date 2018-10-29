0748e3a2327b4bcc62e127fb1035b1bd2d1ba880
jgierlach <jegierlach@gmail.com>
2018-10-28 22:05:33 -0400

    added favicon of ParkShark logo

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ParkSharkCensor from 'ParkSharkCensor.png'
+// import ParkSharkCensor from 'ParkSharkCensor.png'
 import './blog.css'
 
 const blog = () => {
@@ -10,7 +10,7 @@ const blog = () => {
           <div className="col s12 m4 center margin">
             <div id="margin" className="card margin">
               <div className="card-image">
-                <img src={ParkSharkCensor} alt=""/>
+                {/* <img src={ParkSharkCensor} alt=""/> */}
                 {/* <span class="card-title">Card Title</span> */}
               </div>
         <div className="card-content">
