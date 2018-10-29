ded8640de74c5650f9678fcc46cd7c76c46b59fa
jgierlach <jegierlach@gmail.com>
2018-10-29 19:26:12 -0400

    added google tag manager and google analytics

--- src/blog.js
+++ src/blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import ParkSharkCensor from 'ParkSharkCensor.png'
+// import ParkingCensor from 'ParkingCensor.jpg'
 import './blog.css'
 
 const blog = () => {
@@ -10,7 +10,7 @@ const blog = () => {
           <div className="col s12 m4 center margin">
             <div id="margin" className="card margin">
               <div className="card-image">
-                {/* <img src={ParkSharkCensor} alt=""/> */}
+                <img alt=""/>
                 {/* <span class="card-title">Card Title</span> */}
               </div>
         <div className="card-content">
