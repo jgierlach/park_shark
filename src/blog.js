df182d3ead85bb0172b508f06404c85b49e8f2f5
jgierlach <jegierlach@gmail.com>
2018-12-06 21:58:02 -0500

    centered the one article card in the blog component

--- src/blog.js
+++ src/blog.js
@@ -4,18 +4,19 @@ import './blog.css'
 
 const blog = () => {
     return (
-     <a href="https://www.hollandsentinel.com/news/20181021/hope-students-offer-solution-to-downtown-holland-parking"> 
-      <div className="container">
-        <div id="margin" className="row margin">
-          <div className="col s12 m4 center margin">
-            <div id="margin" className="card margin">
+     <a href="https://www.hollandsentinel.com/news/20181021/hope-students-offer-solution-to-downtown-holland-parking">
+     {/* Some messy inline styles to get the one article to center on the page must clean this up later  */}
+      <div style={{marginTop: "20px", width: "100%", display: "flex", justifyContent: "center"}} className="container">
+        <div className="row center">
+          <div className="col s12 center">
+            <div className="card">
               <div className="card-image">
                 <img alt=""/>
                 {/* <span class="card-title">Card Title</span> */}
               </div>
-        <div className="card-content">
-          <p>Park Shark in the Holland Sentinal.</p>
-        </div>
+             <div className="card-content">
+              <p>Park Shark in the Holland Sentinal.</p>
+            </div>
             <div className="card-action">
                Read Article
            </div>
